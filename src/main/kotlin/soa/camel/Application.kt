4708dbfbc966111dd65ec4feae1541a9b7ff67af
Felipe <fnonay@gmail.com>
2021-12-16 00:27:03 +0100

    Primary goal

--- src/main/kotlin/soa/camel/Application.kt
+++ src/main/kotlin/soa/camel/Application.kt
@@ -43,6 +43,11 @@ class Router(meterRegistry: MeterRegistry) : RouteBuilder() {
 
     override fun configure() {
         from(DIRECT_ROUTE)
+            .process { exchange ->
+                val q = exchange.getIn().getHeader("keywords") as String
+                val qFmt = q.replace("max:", "?count=")
+                exchange.getIn().setHeader("keywords", qFmt)
+            }
             .toD("twitter-search:\${header.keywords}")
             .wireTap(LOG_ROUTE)
             .wireTap(COUNT_ROUTE)
